b37a97f08bd7a51810c3ff3989b5fc3f8e7751d7
e.tyutyuev <e.tyutyuev@2gis.ru>
2016-03-20 16:56:21 +0600

    Initial Commit

---
+++ Scheduler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scheduler
+//
+//  Created by Eugene Tutuev on 20/03/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SchedulerTests/SchedulerTests.m
@@ -0,0 +1,39 @@
+//
+//  SchedulerTests.m
+//  SchedulerTests
+//
+//  Created by Eugene Tutuev on 20/03/16.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SchedulerTests : XCTestCase
+
+@end
+
+@implementation SchedulerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
